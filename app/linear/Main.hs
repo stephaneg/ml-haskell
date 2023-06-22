69b21fcece55310006e8a0136a06a189002050a9
Stephane <stephstephg@gmail.com>
2023-06-22 23:00:49 +0200

    loss function

--- app/linear/Main.hs
+++ app/linear/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
+predict:: [Double] -> Double -> Double ->[Double]
+predict l w b = map (\x -> x * w + b) l
+
+loss:: [Double] -> [Double] -> Double -> Double -> [Double]
+loss l y w b = 
+        let d = zipwidth (-) (predict x w b) y
+        in 
 
 main :: IO ()
 main = do
         putStrLn "Linear Regression"
+        let x = [1,2,3,4,5,6]
+        let r = predict x 2 3
+        putStrLn $ show r
+
+
