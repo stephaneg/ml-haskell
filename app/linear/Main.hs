43c91d4e9e1a92c634458472c8a863866117df44
Stephane <stephstephg@gmail.com>
2023-06-29 22:52:53 +0200

    code predict and loss

--- app/linear/Main.hs
+++ app/linear/Main.hs
@@ -1,12 +1,20 @@
 module Main where
 
+
 predict:: [Double] -> Double -> Double ->[Double]
 predict l w b = map (\x -> x * w + b) l
 
-loss:: [Double] -> [Double] -> Double -> Double -> [Double]
-loss l y w b = 
-        let d = zipwidth (-) (predict x w b) y
-        in 
+
+avgLst::[Double]->Double
+avgLst l = (sum l) / (fromIntegral $ length l) 
+
+
+loss:: [Double] -> [Double] -> Double -> Double -> Double
+loss x y w b = 
+        let d = zipWith (-) (predict x w b) y 
+        in avgLst (map (\e -> e * e) d)
+
+
 
 main :: IO ()
 main = do
@@ -14,5 +22,7 @@ main = do
         let x = [1,2,3,4,5,6]
         let r = predict x 2 3
         putStrLn $ show r
+        putStrLn $ show $ avgLst x
+
 
 
